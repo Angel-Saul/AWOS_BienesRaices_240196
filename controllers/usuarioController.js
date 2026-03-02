4cd7385141cb1452418721c2145295d95dde727f
Angel Saúl <240196@utxicotepec.edu.mx>
2026-03-02 11:54:59 -0600

    PC - Bienes Raices - Seccion06
    add: Se agregó la librería para generación de tokens, update: Se actualizaron las validaciones de usuarios únicos en correo electrónico en base de datos y la respuesta exitosa de creación de cuenta

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,5 +1,6 @@
 import Usuario from '../models/usuario.js';
 import { check, validationResult } from 'express-validator';
+import { generarToken } from '../lib/token.js';
 
 const formularioLogin = (req, res) => {
     res.render('auth/login', { pagina: "Ingresa los datos de la cuenta" });
@@ -20,33 +21,54 @@ const formualrioRecuperar = (req, res) => {
 
 const registrarUsuario = async (req, res) => {
     console.log("Intentando registrar un nuevo usuario:");
-    
-    const {name, email, password } = req.body;
+
+    const { nombreUsuario: nombre, emailUsuario: email, passwordUsuario: password } = req.body;
 
     // Validación de los datos del formulario previo a registro en la BD 
     await check('nombreUsuario').notEmpty().withMessage("El nombre de la persona no puede ser vacio").run(req);
     await check('emailUsuario').notEmpty().withMessage("El correo electronico no puede ser vacio").isEmail().withMessage("El correo no tiene formato adecuado").run(req);
     await check('passwordUsuario').notEmpty().withMessage("La contraseña no puede ser vacia").isLength({ min: 8, max: 30 }).withMessage("La longitud de la contraseña debe ser entre 8 y 30 caracteres").run(req);
-    await check("confirmUsuario").equals(req.body.passwordUsuario).withMessage("Ambas contraseñas deben de ser iguales").run(req);
+    await check("confirmUsuario").equals(password).withMessage("Ambas contraseñas deben de ser iguales").run(req);
 
     // aplicamos las reglas definidas
-    let resultadoValidacion = validationResult(req); 
+    let resultadoValidacion = validationResult(req);
+
+    //Verificar si el usuario no está previamente registrado en la bd 
+    const existeUsuario = await Usuario.findOne({ where: { email } })
+
+
+    if (existeUsuario) {
+        res.render("auth/registro", {
+            pagina: "Registrate con nosotros :)",
+            errores: [{ msg: `Ya existe el usuario asociado al correo: ${email}` }],
+            usuario: {
+                nombreUsuario: nombre,
+            }
+        });
+    }
+
+
 
     if (resultadoValidacion.isEmpty()) {
         const data = {
-            nombre: req.body.nombreUsuario,
-            email: req.body.emailUsuario,
-            password: req.body.passwordUsuario
+            nombre,
+            email,
+            password,
+            token: generarToken()
         }
         const usuario = await Usuario.create(data);
-        res.json(usuario);
+        res.render("template/mensaje", {
+            pagina: "Bienvenid@ a BienesRaices!",
+            mensaje: `La cuenta asociada al correo ${email}, se ha creado satisfactoriamente, 
+            te pedimos confirmar tu cuenta a través del correo electronico que te hemos mandado`
+        });
     } else {
         res.render('auth/registro', {
-            pagina: "Error al intentar crear una cuenta", 
+            pagina: "Error al intentar crear una cuenta",
             errores: resultadoValidacion.array(),
             usuario: {
-                nombreUsuario: req.body.nombreUsuario,
-                emailUsuario: req.body.emailUsuario
+                nombreUsuario: nombre,
+                emailUsuario: email
             }
         });
     }
@@ -54,9 +76,9 @@ const registrarUsuario = async (req, res) => {
 
 // IMPORTANTE: Exportar la nueva función formularioRegistro2
 export {
-    formularioLogin, 
-    formularioRegistro, 
-    formularioRegistro2, // <--- Esta es la clave
-    formualrioRecuperar, 
+    formularioLogin,
+    formularioRegistro,
+    formularioRegistro2, 
+    formualrioRecuperar,
     registrarUsuario
 }
